6ce9a7441d3c4624750a03b5c081e079d2f81233
riya-rjha <riyaranjanjha2006@gmail.com>
2024-03-24 20:31:57 +0530

    Implementing Logout functionality

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -1,16 +1,44 @@
 // NavBar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+
+    const [cookies, setCookies] = useCookies(['access_token']);
+    const navigate = useNavigate();
+
+    const logout = () => {
+        setCookies('access_token', "");
+        window.localStorage.removeItem('userID');
+        navigate('/');
+    }
+
     return (
         <nav className="bg-gray-800 py-4">
             <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-white text-lg font-semibold">Recipe App</Link>
-                <div className="flex items-center">
-                    <Link to="/auth" className="text-white mr-4 hover:text-gray-300">Authentication</Link>
-                    <Link to="/create" className="text-white mr-4 hover:text-gray-300">Create Recipes</Link>
-                    <Link to="/saved" className="text-white hover:text-gray-300">Saved Recipes</Link>
+                <Link
+                    to="/"
+                    className="text-white text-lg font-semibold">
+                    Recipe App
+                </Link>
+                <div className="flex items-center justify-center">
+                    <Link
+                        to="/create"
+                        className="text-white mr-4 hover:text-gray-300">
+                        Create Recipes
+                    </Link>
+                    <Link
+                        to="/saved"
+                        className="text-white mr-4 hover:text-gray-300">
+                        Saved Recipes
+                    </Link>
+                    {!cookies.access_token ? (<Link to="/auth" className="text-white mr-4 hover:text-gray-300">Authentication</Link>
+                    ) : (<button onClick={logout}
+                        className='text-white mr-12 hover:text-gray-300'>
+                        Logout
+                    </button>)}
                 </div>
             </div>
         </nav>
